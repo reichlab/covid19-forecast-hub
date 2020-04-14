ad74328dabafd1530ed7436585b6e467961ca176
Nicholas Reich <nick@schoolph.umass.edu>
2020-04-14 10:53:31 -0400

    update to LANL file format to standardize timezeroes and targets with day/weekaheads from timezeroes. closes #17

--- code/LANL-processing-script.R
+++ code/LANL-processing-script.R
@@ -6,11 +6,14 @@ library(tidyverse)
 
 source("code/process_lanl_file.R")
 
-files_to_munge <- list.files("data-raw/LANL/", pattern="*deaths_quantiles_us.csv", )
+files_to_munge <- tibble(
+    filenames=list.files("data-raw/LANL/", pattern="*deaths_quantiles_us.csv", ),
+    timezeroes = c("2020-04-06", "2020-04-13", "2020-04-13")
+)
 
-for(i in 1:length(files_to_munge)) {
-    forecast_date <- substr(basename(files_to_munge[i]), 0, 10)
-    tmp_dat <- process_lanl_file(file.path("data-raw/LANL/",files_to_munge[i]))    
-    write_csv(tmp_dat, paste0("data-processed/LANL-GrowthRate/", forecast_date, "-LANL-GrowthRate.csv"))
+
+for(i in 1:nrow(files_to_munge)) {
+    tmp_dat <- process_lanl_file(file.path("data-raw/LANL/",files_to_munge$filenames[i]), timezero=files_to_munge$timezeroes[i])    
+    write_csv(tmp_dat, paste0("data-processed/LANL-GrowthRate/", files_to_munge$timezeroes[i], "-LANL-GrowthRate.csv"))
 }
 
--- code/process_lanl_file.R
+++ code/process_lanl_file.R
@@ -21,9 +21,13 @@ get_next_saturday <- function(date) {
 #' turn LANL forecast file into quantile-based format
 #'
 #' @param lanl_filepath path to a lanl submission file
+#' @param timezero the origin date for the forecast
+#'
+#' @details typically timezero will be a monday and the 1-week ahead 
+#' forecast will be for the EW of the Monday. 1-day-ahead would be Tuesday.
 #'
 #' @return a data.frame in quantile format
-process_lanl_file <- function(lanl_filepath) {
+process_lanl_file <- function(lanl_filepath, timezero) {
     require(tidyverse)
     require(MMWRweek)
     require(lubridate)
@@ -34,9 +38,17 @@ process_lanl_file <- function(lanl_filepath) {
     ## read in FIPS codes
     fips <- read_csv("template/state_fips_codes.csv")
     
+    ## read in forecast dates
+    fcast_dates <- read_csv("template/covid19-death-forecast-dates.csv")
+    timezero <- as.Date(timezero)
+    
     ## read in data
     dat <- read_csv(lanl_filepath)
     forecast_date <- unique(dat$fcst_date)
+    
+    diff_in_fcast_dates <- timezero - forecast_date 
+    if(diff_in_fcast_dates<0)
+        stop("timezero is before the forecast date")
     
     ## put into long format
     dat_long <- pivot_longer(dat, cols=starts_with("q."), names_to = "q", values_to = "cum_deaths") %>%
@@ -50,8 +62,8 @@ process_lanl_file <- function(lanl_filepath) {
             value = cum_deaths)
     
     ## create tables corresponding to the days for each of the targets
-    day_aheads <- tibble(target_id = paste(1:7, "day ahead cum"), dates = forecast_date+1:7)
-    week_aheads <- tibble(target_id = paste(1:7, "wk ahead cum"), dates = get_next_saturday(forecast_date+seq(0, by=7, length.out = 7)))
+    day_aheads <- tibble(target_id = paste(1:7, "day ahead cum"), dates = timezero+1:7)
+    week_aheads <- tibble(target_id = paste(1:7, "wk ahead cum"), dates = get_next_saturday(timezero+seq(0, by=7, length.out = 7)))
     
     ## merge so targets are aligned with dates
     fcast_days <- inner_join(day_aheads, dat_long) %>% select(-dates)
