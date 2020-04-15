282747e74216ddebde284e35e5902bef19b65270
Johannes Bracher <you@example.com>
2020-04-15 11:00:23 +0200

    Updating scripts to process CU files.

--- code/CU-processing-script.R
+++ code/CU-processing-script.R
@@ -2,21 +2,14 @@
 ## Johannes Bracher
 ## April 2020
 
-##############################################
-# This is only for demonstration purposes right now as
-# the April 12 file is damaged
-##############################################
-
 source("code/process_cu_file.R")
 
-# !!! Using April 09 for demonstration as April 12 csvs are somehow faulty (contain extra columns)
-# uncomment the following lines to test with April 9 data
-# folders_to_process <- data.frame(
-#   folders = as.character(c("Projection_April09/cdc_hosp")),
-#   timezeroes = as.Date(c("2020-04-13"))
-# )
+folders_to_process <- data.frame(
+  folders = as.character(c("Projection_April12/cdchosp")),
+  timezeroes = as.Date(c("2020-04-13"))
+)
 
-# names of the files that should be contained in a CU folder:
+# different versions of CU forecasts:
 scenarios <- c("60contact", "70contact", "80contact", "nointerv")
 
 # check whether all timezero values are valid:
@@ -30,7 +23,7 @@ for(i in 1:nrow(folders_to_process)) {
   # check if file naming still the same:
   tmp_ls_files <- list.files(paste0("data-raw/CU/",
                                     folders_to_process$folders[i]))
-  if(!all(paste0("cdchosp_", scenarios, ".csv") %in% tmp_ls_files)){
+  if(!all(paste0("state_cdchosp_", scenarios, ".csv") %in% tmp_ls_files)){
     stop("Not all expected files found.")
   }
 
@@ -40,7 +33,7 @@ for(i in 1:nrow(folders_to_process)) {
 
     tmp_dat <- process_cu_file(
       cu_filepath = file.path("data-raw/CU/", folders_to_process$folders[i]),
-      file = paste0("cdchosp_", sc, ".csv"),
+      file = paste0("state_cdchosp_", sc, ".csv"),
       timezero = folders_to_process$timezeroes[i])
 
     write.csv(tmp_dat, paste0("data-processed/CU-", sc, "/",
--- code/process_cu_file.R
+++ code/process_cu_file.R
@@ -4,29 +4,25 @@
 
 #' turn CU forecast file into quantile-based format
 #'
-#' NOTE: only extracts incident deaths, will need to be adapted once cumulative deaths become available
-#'
 #' @param cu_filepath path to a cu submission file
-#' @param file the name of the file (CU forecast files are bed_60contact.csv,
-#' bed_70contact.csv, bed_80contact.csv, bed_nointerv.csv)
+#' @param file the name of the file (CU forecast files are state_cdchosp_60contact.csv,
+#' state_cdchosp_70contact.csv, state_cdchosp_80contact.csv, state_cdchosp_nointerv.csv)
 #' @param timezero the origin date for the forecast
-#' @param exclude_counties should county-level forecasts be excluded and only state
-#'  and national forecasts kept? Defaults to TRUE
 #'
-#' @details typically timezero will be a monday and the 1-week ahead
+#' @details typically timezero will be a Monday and the 1-week ahead
 #' forecast will be for the EW of the Monday. 1-day-ahead would be Tuesday.
 #'
 #' @return a data.frame in quantile format
 
-process_cu_file <- function(cu_filepath, file, timezero, exclude_counties = TRUE) {
+process_cu_file <- function(cu_filepath, file, timezero) {
 
   timezero <- as.Date(timezero)
 
-  # extract forecast date from path:
+  # extract CU forecast date from path:
   forecast_date <- as.Date(paste0(strsplit(strsplit(cu_filepath, "Projection_")[[1]][2],
                                            "/", fixed = TRUE)[[1]][1], "/2020"),
                            format = "%B%d/%Y")
-  # plausibility check:
+  # plausibility check: is forecast date before timezero, but not too much?
   if(is.na(forecast_date)){
     warning("forecast date could not be extracted from cu_filepath")
   }else{
@@ -41,36 +37,39 @@ process_cu_file <- function(cu_filepath, file, timezero, exclude_counties = TRUE
   # format date variable:
   dat$Date <- as.Date(dat$Date, format = "%m/%d/%y")
 
-  # get rid of unnecessary columns:
-  death_vars <- colnames(dat)[grepl(colnames(dat), pattern = "death")]
-  dat <- dat[, c("location", "fips", "Date", death_vars)]
-
-  # restrict to state and national-level forecasts
-  if(exclude_counties){
-    dat <- subset(dat, nchar(fips) < 3)
-  }
-
-
-  dat <- reshape(dat, direction = "long", varying = list(death_vars), times = c(2.5, 25, 50, 75, 97.5))
-  # adapt columns and their names to template
-  dat$id <- NULL
-  colnames(dat) <- c("location_name", "location_id", "date", "quantile", "value")
-  rownames(dat) <- NULL
-  dat$type <- "quantile"
-
-
-  # bring quantiles to unit interval
-  dat$quantile <- dat$quantile/100
-
   # restrict to timepoints after time of collection:
-  dat <- subset(dat, date > timezero)
+  dat <- subset(dat, Date > timezero)
 
-  # create target variable:
-  # !!! This will need to be adapted when cumulative deaths become available
-  dat$target_id <- paste(dat$date - timezero, "day ahead inc")
+  # identify columns with death forecasts:
+  all_death_vars <- colnames(dat)[grepl(colnames(dat), pattern = "death")]
+  c_death_vars <- colnames(dat)[grepl(colnames(dat), pattern = "cdeath")]
+  i_death_vars <- setdiff(all_death_vars, c_death_vars)
 
-  # remove date variable:
+  # split into incident and cumulative, reshape each:
+  # cumulative:
+  c_dat <- dat[, c("location", "fips", "Date", c_death_vars)]
+  c_dat <- reshape(c_dat, direction = "long", varying = list(c_death_vars),
+                 times = c(1, 2.5, seq(from = 5, to = 95, by = 5), 97.5, 99)/100)
+  c_dat$id <- NULL
+  c_dat$target <- paste(c_dat$Date - timezero, "day ahead cum")
+
+  # incident:
+  i_dat <- dat[, c("location", "fips", "Date", i_death_vars)]
+  i_dat <- reshape(i_dat, direction = "long", varying = list(i_death_vars),
+                   times = c(1, 2.5, seq(from = 5, to = 95, by = 5), 97.5, 99)/100)
+  i_dat$id <- NULL
+  i_dat$target <- paste(i_dat$Date - timezero, "day ahead inc")
+
+  # adapt columns and their names to template
+  colnames(c_dat) <- colnames(i_dat) <- c("location_name", "location", "date",
+                                          "quantile", "value", "target")
+
+  # put together and tidy up:
+  dat <- rbind(c_dat, i_dat)
   dat$date <- NULL
+
+  # add type variable (all quantiles until here):
+  dat$type <- "quantile"
 
   # add medians as point estimates:
   medians <- subset(dat, quantile == 0.5)
@@ -79,9 +78,11 @@ process_cu_file <- function(cu_filepath, file, timezero, exclude_counties = TRUE
 
   # add to data:
   dat <- rbind(dat, medians)
+  rownames(dat) <- NULL
+
   # re-order:
-  dat <- dat[order(dat$target_id, dat$type),
-             c("location_id", "location_name", "target_id", "type", "quantile", "value")]
+  dat <- dat[order(dat$target, dat$type),
+             c("location", "location_name", "target", "type", "quantile", "value")]
 
   return(dat)
 }
