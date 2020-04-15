eff046b8fa3c104c08c93336dc757ff63dab5608
Nicholas Reich <nick@schoolph.umass.edu>
2020-04-14 22:16:17 -0400

    adding MOBS data.

---
+++ code/MOBS-processing.R
@@ -0,0 +1,16 @@
+## NEU processing
+## one-off updates for 4/13 file.
+
+dat <- read_csv("data-raw/MOBS/2020-04-13-MOBS_NEU-GLEAM-COVID-19_v1.csv") %>%
+    rename(target=target_id, location=location_id) %>%
+    mutate(location = str_pad(as.character(location), width = 2, side = "left", pad = "0"))
+
+point_ests <- dat %>% 
+    filter(quantile==0.5) %>% 
+    mutate(quantile=NA, type="point")
+
+all_dat <- bind_rows(dat, point_ests) %>%
+    arrange(type, target, quantile) 
+
+write_csv(all_dat, "data-processed/MOBS/2020-04-13-MOBS_NEU-GLEAM_COVID_19_v1.csv")
+
