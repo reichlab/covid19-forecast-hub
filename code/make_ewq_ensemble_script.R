b72aeb2a8a210dedd6b65e67718bd91a54184c97
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2020-04-18 12:57:35 -0400

    ensemble component data

--- code/make_ewq_ensemble_script.R
+++ code/make_ewq_ensemble_script.R
@@ -17,10 +17,10 @@ for(i in death_files){
 }
 write.csv(death_info_file, file="./template/death_forecast-model-infomation.csv",row.names = FALSE)
 # check<-unique(death_info_file[ ,3:4])
-ensemble_info <- death_info_file
-# make ensemble
+
+# ----------------------------  make ensemble ---------------------------- #
 ## state
-models <- c("LANL","IHME")
+models <- c("LANL-GrowthRate","IHME-CurveFit")
 combined_table <- pull_all_forecasts(this_date,models,"wk ahead cum",quantiles=c(0.025,0.5,0.975)) %>%
   dplyr::filter(target!="7 wk ahead cum death",location!="US")
 # adding manual check
@@ -32,8 +32,9 @@ mismatched_location <- unique(check_table$location[which(check_table$n!=length(m
 combined_table <- combined_table %>%
   dplyr::filter(location!=78&location!=72)
 quant_ensemble<-ew_quantile(combined_table, quantiles=c(0.025,0.5,0.975),national=FALSE)
+
 ## national
-models_n <- c("CU-60contact","CU-70contact","CU-80contact","IHME")
+models_n <- c("CU-60contact","CU-70contact","CU-80contact","IHME-CurveFit")
 combined_table_n <- pull_all_forecasts(this_date,models_n,"wk ahead cum",quantiles=c(0.025,0.5,0.975)) %>%
   dplyr::filter(target!="7 wk ahead cum death",location=="US") 
 # adding manual check
@@ -43,7 +44,8 @@ check_table_n <-combined_table_n %>%
 mismatched_location_n <- unique(check_table_n$location[which(check_table_n$n!=length(models_n))])
 # excluding mismatched location if any and run
 quant_ensemble_n<-ew_quantile(combined_table_n, quantiles=c(0.025,0.5,0.975),national=TRUE)
-## combine state and national
+
+## -------- combine state and national -------- ##
 final_ens <- rbind(quant_ensemble_n,quant_ensemble)
 # format code
 final_ens$location[which(nchar(final_ens$location)==1)] <- paste0(0,final_ens$location[which(nchar(final_ens$location)==1)])
@@ -56,3 +58,26 @@ mismatched <- unique(check_table2$location[which(check_table2$n!=1)])
 verify_quantile_forecasts(final_ens)
 write.csv(final_ens,file=paste0("./data-processed/COVIDhub-ensemble/",this_date,"-COVIDhub-ensemble.csv"),
             row.names = FALSE)
+
+
+## -------------------- write ensemble info ----------------------------##
+# need to change weekly
+ensemble_info <- data.frame(
+  cbind(c(rep("US national-level",length(models_n)),rep("state-level",length(models))),
+                                  c(models_n,models),
+                                  rep("0.025,0.5,0.975",length(c(models_n,models))),
+                                  rep(this_date,length(c(models_n,models))),
+                                  # weight
+                                  c(rep(1/length(models_n),length(models_n)),rep(1/length(models),length(models))),
+                                  # target
+                                  rep("1-6 wk ahead cum death",length(c(models_n,models)))
+        )
+  )
+  
+names(ensemble_info) <- c("location","model_name","quantile","forecast_colleceted_date","weight","target")
+
+# read in previous data
+preinfo <- read.csv("./data-processed/COVIDhub-ensemble/COVIDhub-ensemble-information.csv",stringsAsFactors = FALSE)
+all_info <- rbind(preinfo,ensemble_info)
+write.csv(all_info,file=paste0("./data-processed/COVIDhub-ensemble/COVIDhub-ensemble-information.csv"),
+          row.names = FALSE)
