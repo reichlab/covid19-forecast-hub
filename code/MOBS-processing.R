13d9f9aac41cf53aa7e571693c60cd7c8baa7168
Nicholas Reich <nick@schoolph.umass.edu>
2020-04-20 22:45:22 -0400

    updated MOBS data to address forecast_date issues and new columns. addresses #36.

--- code/MOBS-processing.R
+++ code/MOBS-processing.R
@@ -1,9 +1,17 @@
 ## NEU processing
 ## one-off updates for 4/13 file.
 
-dat <- read_csv("data-raw/MOBS/2020-04-13-MOBS_NEU-GLEAM-COVID-19_v1.csv") %>%
+library(tidyverse)
+library(lubridate)
+
+mobs_filename <- list.files("data-raw/MOBS", pattern=".csv", full.names=TRUE)
+
+forecast_date <- as.Date(substr(basename(mobs_filename), 0, 10))
+    
+dat <- read_csv(mobs_filename) %>%
     rename(target=target_id, location=location_id) %>%
     mutate(
+        forecast_date = forecast_date,
         location = str_pad(as.character(location), width = 2, side = "left", pad = "0"),
         target = paste(target, "death"))
 
@@ -12,12 +20,27 @@ dat$location[US_loc_idx] <- "US"
 
 dat$target <- sub("week", "wk", dat$target)
 
-point_ests <- dat %>% 
+## add target_end_dates
+day_aheads <- tibble(target = paste(1:7, "day ahead cum death"), target_end_date = forecast_date+1:7)
+if(wday(forecast_date) <= 2 ) { ## sunday = 1, ..., saturday = 7
+    ## if timezero is Sun or Mon, then the current epiweek ending on Saturday is the "1 week-ahead"
+    week_aheads <- tibble(target = paste(1:7, "wk ahead cum death"), target_end_date = get_next_saturday(forecast_date+seq(0, by=7, length.out = 7)))
+} else {
+    ## if timezero is after Monday, then the next epiweek is "1 week-ahead"
+    week_aheads <- tibble(target = paste(0:7, "wk ahead cum death"), target_end_date = get_next_saturday(forecast_date+seq(0, by=7, length.out = 8)))
+}
+
+joined_dat <- inner_join(bind_rows(day_aheads, week_aheads), dat) 
+
+
+point_ests <- joined_dat %>% 
     filter(quantile==0.5) %>% 
     mutate(quantile=NA, type="point")
 
-all_dat <- bind_rows(dat, point_ests) %>%
-    arrange(type, target, quantile) 
+all_dat <- bind_rows(joined_dat, point_ests) %>%
+    arrange(type, target, quantile) %>%
+    select(forecast_date, target, target_end_date, location, location_name, type, quantile, value)
 
-write_csv(all_dat, "data-processed/MOBS_NEU-GLEAM/2020-04-13-MOBS_NEU-GLEAM_COVID_19_v1.csv")
+
+write_csv(all_dat, "data-processed/MOBS_NEU-GLEAM_COVID/2020-04-13-MOBS_NEU-GLEAM_COVID.csv")
 
