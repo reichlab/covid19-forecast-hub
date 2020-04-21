8d0a371ea468a239879a0424eb520db32cb4fbef
Johannes Bracher <you@example.com>
2020-04-21 13:38:13 +0200

    Adapting Geneva scripts to new format requirements of processed-data.

--- code/Geneva-processing-script.R
+++ code/Geneva-processing-script.R
@@ -6,24 +6,16 @@ source("code/process_geneva_file.R")
 
 dir.create("data-processed/Geneva-DeterministicGrowth", showWarnings = FALSE)
 
-# no forecast available for 13 April, can only be used from 20 April on (below code is for testing)
-# files_to_process <- data.frame(
-#   filenames = as.character(c("predictions_deaths_2020-04-15.csv")),
-#   timezeroes = as.Date(c("2020-04-15"))
-# )
-
-# check whether all timezero values are valid:
-templ <- read.csv("template/covid19-death-forecast-dates.csv", stringsAsFactors = FALSE)
-templ$timezero <- as.Date(templ$timezero)
-if(!all(as.Date(files_to_process$timezeroes) %in% templ$timezero)){
-  stop("At least one timezero provided in folders_to_process is not valid.")
-}
+files_to_process <- list.files("data-raw/Geneva", recursive = FALSE)
+files_to_process <- files_to_process[grepl(".csv", files_to_process) &
+                                       grepl("predictions_death", files_to_process)]
+forecast_dates <- lapply(files_to_process, date_from_geneva_filepath)
 
 # proces files:
-for(i in 1:nrow(files_to_process)) {
-  tmp_dat <- process_geneva_file(files_to_process$filenames[i], timezero=files_to_process$timezeroes[i])
+for(i in 1:length(files_to_process)) {
+  tmp_dat <- process_geneva_file(files_to_process[i], forecast_date = forecast_dates[[i]])
   write.csv(tmp_dat,
-            paste0("data-processed/Geneva-DeterministicGrowth/", files_to_process$timezeroes[i],
+            paste0("data-processed/Geneva-DeterministicGrowth/", forecast_dates[[i]],
                    "-Geneva-DeterministicGrowth.csv"),
             row.names = FALSE)
 }
--- code/process_geneva_file.R
+++ code/process_geneva_file.R
@@ -2,29 +2,36 @@
 ## Johannes Bracher
 ## April 2020
 
+#' helper funtion to extract a date from a CU path name
+#'
+#' @param geneva_filepath the path from which to extract the date
+#'
+#' @return an object of class date
+
+date_from_geneva_filepath <- function(geneva_filepath){
+  as.Date(gsub(".csv", "", gsub("predictions_deaths_", "", geneva_filepath)))
+}
+
 #' turn Geneva forecast file into quantile-based format
 #'
 #' @param geneva_filepath path to a Geneva submission file
-#' @param file the name of the file
-#' @param timezero the origin date for the forecast
+#' @param forecast_date the time at which the forecast was issued; is internally compared
+#'  to date indicated in file name
 #'
 #' @details typically timezero will be a Monday and the 1-week ahead
 #' forecast will be for the EW of the Monday. 1-day-ahead would be Tuesday.
 #'
 #' @return a data.frame in quantile format
 
-process_geneva_file <- function(geneva_filepath, timezero){
+process_geneva_file <- function(geneva_filepath, forecast_date){
 
-  timezero <- as.Date(timezero)
   # extract Geneva forecast date from path:
-  forecast_date <- as.Date(gsub(".csv", "", gsub("predictions_deaths_", "", geneva_filepath)))
-
-  # plausibility check: is forecast date before timezero, but not too much?
-  if(is.na(forecast_date)){
+  check_forecast_date <- date_from_geneva_filepath(geneva_filepath)
+  # plausibility check: does forecast_date agree with file name?
+  if(is.na(check_forecast_date)){
     warning("forecast date could not be extracted from geneva_filepath")
   }else{
-    if(timezero != forecast_date) warning("Geneva forecasts are usually issued every day. Is a forecast from day timezero availalble?")
-    if(forecast_date > timezero) stop("timezero is before the forecast date")
+    if(check_forecast_date != forecast_date) stop("forecast_date and date in file name differ.")
   }
 
   dat <- read.csv(paste0("data-raw/Geneva/", geneva_filepath))
@@ -32,31 +39,62 @@ process_geneva_file <- function(geneva_filepath, timezero){
   # restrict to US, format:
   dat <- subset(dat, country == "United States of America" & observed == "Predicted")
   dat$date <- as.Date(dat$date)
-  dat <- subset(dat, date > timezero) # restrict to timepoints after collection date
+  dat <- subset(dat, date > forecast_date) # restrict to timepoints after forecast date
   dat$location <- dat$location_name <- "US"
   dat$country <- NULL
   dat$X <- dat$observed <- NULL
 
   # transform to wide format, tidy up:
-  dat_wide <- reshape(dat, direction = "long", varying = list(c("per.day", "cumulative")),
+  daily_dat <- reshape(dat, direction = "long", varying = list(c("per.day", "cumulative")),
                  times = c("day ahead inc death", "days ahead cum death"))
-  dat_wide$id <- NULL
-  rownames(dat_wide) <- NULL
+  daily_dat$id <- NULL
+  rownames(daily_dat) <- NULL
 
   # get forecast horizons:
-  dat_wide$horizon <- as.numeric(dat_wide$date - timezero)
-  dat_wide$target <- paste(dat_wide$horizon, dat_wide$time)
+  daily_dat$horizon <- as.numeric(daily_dat$date - forecast_date)
+  daily_dat$target <- paste(daily_dat$horizon, daily_dat$time)
 
   # remove unneeded columns
-  dat_wide$date <- dat_wide$time <- dat_wide$horizon <- NULL
+  daily_dat$time <- daily_dat$horizon <- NULL
 
   # add required ones:
-  dat_wide$quantile <- NA
-  dat_wide$type = "point"
+  daily_dat$quantile <- NA
+  daily_dat$type = "point"
+  daily_dat$forecast_date <- forecast_date
 
   # adapt colnames and order
-  colnames(dat_wide)[colnames(dat_wide) == "per.day"] <- "value"
-  dat_wide <- dat_wide[, c("target", "location", "location_name", "type", "quantile", "value")]
+  colnames(daily_dat)[colnames(daily_dat) == "per.day"] <- "value"
+  colnames(daily_dat)[colnames(daily_dat) == "date"] <- "target_end_date"
+  daily_dat <- daily_dat[, c("forecast_date", "target", "target_end_date", "location",
+                           "location_name", "type", "quantile", "value")]
 
-  return(dat_wide)
+  # add one-week-ahead cumulative forecast if possible:
+  # get day of the week of forecast_date:
+  day <- weekdays(forecast_date, abbreviate = TRUE)
+
+  # When do the one-week-ahead forecast end?
+  next_dates <- seq(from = forecast_date, length.out = 14, by = 1)
+  next_days <- weekdays(next_dates, abbreviate = TRUE)
+  if(day %in% c("Sun", "Mon")){
+    forecast_1_wk_ahead_end <- next_dates[next_days == "Sat"][1]
+  }else{
+    forecast_1_wk_ahead_end <- next_dates[next_days == "Sat"][2]
+  }
+  # Whether the one-week-ahead forecast can be computed depends on the day
+  # the forecasts were issued:
+  if(max(daily_dat$target_end_date) > forecast_1_wk_ahead_end){
+    ends_weekly_forecasts <- data.frame(end = seq(from = forecast_1_wk_ahead_end,
+                                                  by = 7, to = max(daily_dat$target_end_date)))
+    ends_weekly_forecasts$target <- paste(1:nrow(ends_weekly_forecasts), "wk ahead cum death")
+    # restrict to respective cumulative forecasts:
+    weekly_dat <- subset(daily_dat, target_end_date %in% ends_weekly_forecasts$end &
+                           grepl("cum", daily_dat$target))
+    weekly_dat$target <- NULL
+    weekly_dat <- merge(weekly_dat, ends_weekly_forecasts, by.x = "target_end_date", by.y = "end")
+    weekly_dat <- weekly_dat[, colnames(daily_dat)]
+
+    daily_dat <- rbind(daily_dat, weekly_dat)
+  }
+
+  return(daily_dat)
 }
