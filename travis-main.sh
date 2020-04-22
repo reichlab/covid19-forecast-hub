1e1c55ef80f2b63f208541af2dfe70a2140dcd3c
Katie House <katiehouse3@gmail.com>
2020-04-22 15:33:28 -0400

    update packages

--- travis-main.sh
+++ travis-main.sh
@@ -28,6 +28,7 @@ npm install
 sudo apt-get install python3-pandas
 sudo apt install python3-pip
 pip3 install --upgrade setuptools
+pip3 install pymmwr click requests
 pip3 install git+https://github.com/reichlab/zoltpy/
 source ./travis/test.sh
 echo "build complete"
