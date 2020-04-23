62b4de73f6d3f8d20687073e6cf727b6c7d06d37
Nicholas Reich <nick@schoolph.umass.edu>
2020-04-23 16:19:22 -0400

    updated imperial data processing

--- code/processing-fxns/process_imperial_file.R
+++ code/processing-fxns/process_imperial_file.R
@@ -15,16 +15,17 @@ source("./code/processing-fxns/get_next_saturday.R")
 #'
 process_imperial_file <- function(sample_mat, location, timezero, qntls=c(0.01, 0.025, seq(0.05, 0.95, by=0.05), 0.975, 0.99)) {
     require(tidyverse)
+    require(lubridate)
     
     ## create tables corresponding to the days for each of the targets
     day_aheads <- tibble(
         target = paste(1:7, "day ahead inc death"),
         target_cum = paste(1:7, "day ahead cum death"),
-        dates = timezero+1:7)
+        target_end_date = timezero+1:7)
     week_aheads <- tibble(
         target = "1 wk ahead inc death", 
         target_cum = "1 wk ahead cum death", 
-        dates = get_next_saturday(timezero))
+        target_end_date = get_next_saturday(timezero) + (wday(timezero)>2)*7)
  
     ## make cumulative death counts
     obs_data <- read_csv("data-processed/truth-cum-death.csv") %>%
@@ -34,8 +35,8 @@ process_imperial_file <- function(sample_mat, location, timezero, qntls=c(0.01, 
     sample_mat_cum <- matrixStats::rowCumsums(as.matrix(sample_mat)) + last_obs_death
        
     ## indices and samples for incident deaths 
-    which_days <- which(colnames(sample_mat) %in% as.character(day_aheads$dates))
-    which_weeks <- which(colnames(sample_mat) %in% as.character(week_aheads$dates))
+    which_days <- which(colnames(sample_mat) %in% as.character(day_aheads$target_end_date))
+    which_weeks <- which(colnames(sample_mat) %in% as.character(week_aheads$target_end_date))
     samples_daily <- sample_mat[,which_days]
     samples_weekly <- sample_mat[,which_weeks]
     samples_daily_cum <- sample_mat_cum[,which_days]
@@ -44,17 +45,19 @@ process_imperial_file <- function(sample_mat, location, timezero, qntls=c(0.01, 
     ## choosing quantile type=1 b/c more compatible with discrete samples
     ## other choices gave decimal answers
     qntl_daily <- apply(samples_daily, FUN=function(x) quantile(x, qntls, type=1), MAR=2) 
-    colnames(qntl_daily) <- day_aheads$target[which(day_aheads$dates %in% as.Date(colnames(samples_daily)))]
+    colnames(qntl_daily) <- day_aheads$target[which(day_aheads$target_end_date %in% as.Date(colnames(samples_daily)))]
     qntl_daily_long <- as_tibble(qntl_daily) %>%
         mutate(location=location, quantile = qntls, type="quantile") %>%
-        pivot_longer(cols=contains("day ahead"), names_to = "target") 
+        pivot_longer(cols=contains("day ahead"), names_to = "target") %>%
+        inner_join(day_aheads)
     
     ## daily cumulative quantiles
     qntl_daily_cum <- apply(samples_daily_cum, FUN=function(x) quantile(x, qntls, type=1), MAR=2) 
-    colnames(qntl_daily_cum) <- day_aheads$target_cum[which(day_aheads$dates %in% as.Date(colnames(samples_daily)))]
+    colnames(qntl_daily_cum) <- day_aheads$target_cum[which(day_aheads$target_end_date %in% as.Date(colnames(samples_daily)))]
     qntl_daily_cum_long <- as_tibble(qntl_daily_cum) %>%
         mutate(location=location, quantile = qntls, type="quantile") %>%
-        pivot_longer(cols=contains("day ahead"), names_to = "target") 
+        pivot_longer(cols=contains("day ahead"), names_to = "target") %>%
+        inner_join(day_aheads, by=c("target" = "target_cum"))
     
     if(is.null(dim(samples_weekly))){
         ## if only one week
@@ -62,26 +65,30 @@ process_imperial_file <- function(sample_mat, location, timezero, qntls=c(0.01, 
         colnames(qntl_weekly) <- "1 wk ahead inc death"
         qntl_weekly_long <- qntl_weekly %>%
             mutate(location=location, quantile = qntls, type="quantile") %>%
-            pivot_longer(cols=contains("wk ahead"), names_to = "target") 
+            pivot_longer(cols=contains("wk ahead"), names_to = "target") %>%
+            inner_join(week_aheads)
         
         qntl_weekly_cum <- enframe(quantile(samples_weekly_cum, qntls, type=1)) %>% select(value)
         colnames(qntl_weekly_cum) <- "1 wk ahead cum death"
         qntl_weekly_cum_long <- qntl_weekly_cum %>%
             mutate(location=location, quantile = qntls, type="quantile") %>%
-            pivot_longer(cols=contains("wk ahead"), names_to = "target") 
+            pivot_longer(cols=contains("wk ahead"), names_to = "target") %>%
+            inner_join(week_aheads, by=c("target" = "target_cum"))
     } else { 
         ## if there are more than 1 weeks
         qntl_weekly <- apply(samples_weekly, FUN=function(x) quantile(x, qntls, type=1), MAR=2) 
-        colnames(qntl_weekly) <- week_aheads$target[which(week_aheads$dates %in% as.Date(colnames(qntl_weekly)))]
+        colnames(qntl_weekly) <- week_aheads$target[which(week_aheads$target_end_date %in% as.Date(colnames(qntl_weekly)))]
         qntl_weekly_long <- as_tibble(qntl_weekly) %>%
             mutate(location=location, quantile = qntls, type="quantile") %>%
-            pivot_longer(cols=contains("wk ahead"), names_to = "target") 
+            pivot_longer(cols=contains("wk ahead"), names_to = "target") %>%
+            inner_join(week_aheads)
         
         qntl_weekly_cum <- apply(samples_weekly_cum, FUN=function(x) quantile(x, qntls, type=1), MAR=2) 
-        colnames(qntl_weekly_cum) <- week_aheads$target[which(week_aheads$dates %in% as.Date(colnames(samples_weekly)))]
+        colnames(qntl_weekly_cum) <- week_aheads$target[which(week_aheads$target_end_date %in% as.Date(colnames(samples_weekly)))]
         qntl_weekly_cum_long <- as_tibble(qntl_weekly_cum) %>%
             mutate(location=location, quantile = qntls, type="quantile") %>%
-            pivot_longer(cols=contains("wk ahead"), names_to = "target") 
+            pivot_longer(cols=contains("wk ahead"), names_to = "target") %>%
+            inner_join(week_aheads, by=c("target" = "target_cum"))
     }
     
     qntl_dat_long <- bind_rows(
@@ -95,8 +102,8 @@ process_imperial_file <- function(sample_mat, location, timezero, qntls=c(0.01, 
     
     all_dat <- bind_rows(qntl_dat_long, point_ests) %>%
         arrange(type, target, quantile) %>%
-        mutate(quantile = round(quantile, 3)) %>%
-        select(target,location,type,quantile,value)
+        mutate(quantile = round(quantile, 3), forecast_date = timezero) %>%
+        select(forecast_date, target, target_end_date,location,type,quantile,value)
     
     return(all_dat)
 }
--- code/processing-scripts/Imperial-processing.R
+++ code/processing-scripts/Imperial-processing.R
@@ -7,7 +7,8 @@ library(tidyverse)
 source("code/processing-fxns/process_imperial_file.R")
 
 ## this reads in an RDS file provided by the Imperial team  on April 11
-ens_preds <- readRDS("./data-raw/Imperial/20200412-ensemble_model_predictions.rds")
+ens_preds <- readRDS("./data-raw/Imperial/20200405-ensemble_model_predictions.rds")
+forecast_date = as.Date("2020-03-29")
 
 ## the object is a big list, with one element for each of the 5 times forecasts were made
 ## each of those elements is itself a list, with one element for each country
@@ -15,30 +16,20 @@ ens_preds <- readRDS("./data-raw/Imperial/20200412-ensemble_model_predictions.rd
 ## each forecast itself is a matrix with rows as samples (30K) and columns representing days in the future
 
 ## this code produces the mean predicted incident deaths for seven day-ahead
-colMeans(ens_preds$`2020-04-12`$United_States_of_America[[1]])
+## colMeans(ens_preds$`2020-04-05`$United_States_of_America[[1]])
 
-## align forecast dates with timezeros
-imperial_forecast_dates <- tibble(
-    #raw_forecast_date = seq.Date(as.Date("2020-03-15"), by="1 week", length.out = 4),
-    raw_forecast_date = as.Date("2020-04-12"),
-    timezero = raw_forecast_date + 1
-)
-write_csv(imperial_forecast_dates, "data-processed/Imperial-ensemble1/Imperial-forecast-dates.csv")
-write_csv(imperial_forecast_dates, "data-processed/Imperial-ensemble2/Imperial-forecast-dates.csv")
 
 ## transform and write the files for each date
-for(i in 1:nrow(imperial_forecast_dates)){
-    ensemble1_output <- process_imperial_file(
-        ens_preds[[as.character(imperial_forecast_dates$raw_forecast_date[i])]]$United_States_of_America[[1]], 
-        location="US", 
-        timezero=imperial_forecast_dates$timezero[i])
-    ensemble2_output <- process_imperial_file(
-        ens_preds[[as.character(imperial_forecast_dates$raw_forecast_date[i])]]$United_States_of_America[[2]], 
-        location="US", 
-        timezero=imperial_forecast_dates$timezero[i])
-    write_csv(ensemble1_output, 
-        path = paste0("data-processed/Imperial-ensemble1/", imperial_forecast_dates$timezero[i],"-Imperial-ensemble1.csv"))
-    write_csv(ensemble2_output, 
-        path = paste0("data-processed/Imperial-ensemble2/", imperial_forecast_dates$timezero[i],"-Imperial-ensemble2.csv"))
-}
+ensemble1_output <- process_imperial_file(
+    ens_preds[[as.character(forecast_date)]]$United_States_of_America[[1]], 
+    location="US", 
+    timezero=forecast_date)
+ensemble2_output <- process_imperial_file(
+    ens_preds[[as.character(forecast_date)]]$United_States_of_America[[2]], 
+    location="US", 
+    timezero=forecast_date)
+write_csv(ensemble1_output, 
+    path = paste0("data-processed/Imperial-ensemble1/", forecast_date,"-Imperial-ensemble1.csv"))
+write_csv(ensemble2_output, 
+    path = paste0("data-processed/Imperial-ensemble2/", forecast_date,"-Imperial-ensemble2.csv"))
 
