0df9f7f0f14f228a1bab0d63fb636d924787a43b
Khoale1096 <Khoale1096>
2020-04-26 00:55:01 -0400

    test update data 3

--- travis/pull-data.sh
+++ travis/pull-data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Automating pulling of data
-python3 ./code/automate-download/auto-download-ihme-covid19.py /home/data-raw/IHME/
-python3 ./code/automate-download/auto-download-lanl-covid19.py /home/data-raw/LANL/
+python3 ./code/automate-download/auto-download-ihme-covid19.py ./data-raw/IHME/
+python3 code/automate-download/auto-download-lanl-covid19.py $pwd/data-raw/LANL/
