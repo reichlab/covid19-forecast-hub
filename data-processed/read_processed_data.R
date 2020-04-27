7123b67c8a0413b6682726d734d097245f46262a
Jarad Niemi <niemi@iastate.edu>
2020-04-27 10:46:52 -0500

    Read csv using tryCatch to associate warnings to files

--- data-processed/read_processed_data.R
+++ data-processed/read_processed_data.R
@@ -5,17 +5,34 @@ library("tidyr")
 library("readr")
 
 read_my_csv = function(f, into) {
-  print(paste("Reading",f,"\n"))
-  readr::read_csv(f,
-                  col_types = readr::cols(
-                    forecast_date   = readr::col_date(format = ""),
-                    target          = readr::col_character(),
-                    target_end_date = readr::col_date(format = ""),
-                    location        = readr::col_character(),
-                    type            = readr::col_character(),
-                    quantile        = readr::col_double(),
-                    value           = readr::col_double()
-                  )) %>%
+  tryCatch(
+    readr::read_csv(f,
+                    col_types = readr::cols(
+                      forecast_date   = readr::col_date(format = ""),
+                      target          = readr::col_character(),
+                      target_end_date = readr::col_date(format = ""),
+                      location        = readr::col_character(),
+                      type            = readr::col_character(),
+                      quantile        = readr::col_double(),
+                      value           = readr::col_double()
+                    )),
+    warning = function(w) {
+      w$message <- paste0(f,": ", gsub("simpleWarning: ","",w))
+      warning(w)
+      suppressWarnings(
+        readr::read_csv(f,
+                        col_types = readr::cols(
+                          forecast_date   = readr::col_date(format = ""),
+                          target          = readr::col_character(),
+                          target_end_date = readr::col_date(format = ""),
+                          location        = readr::col_character(),
+                          type            = readr::col_character(),
+                          quantile        = readr::col_double(),
+                          value           = readr::col_double()
+                        ))
+      )
+    }
+  ) %>%
     dplyr::mutate(file = f) %>%
     tidyr::separate(file, into, sep="-|/") 
 }
@@ -44,8 +61,6 @@ fips <- read_csv("../template/state_fips_codes.csv",
   bind_rows(dplyr::tibble(fips_alpha = "US", 
                               fips_numeric = "US", 
                               full_name = "United States"))
-
-warning("The following warnings are due to missing required columns:")
 
 all_data = read_my_dir(".", "*.csv",
                 into = c("period","team","model",
