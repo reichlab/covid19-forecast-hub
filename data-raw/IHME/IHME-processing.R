cec9622e0ca4729ddb6cef53f7ba7aaea53b8aad
Jarad Niemi <niemi@iastate.edu>
2020-05-07 12:02:03 -0500

    Move IHME processing file to data-raw/IHME
    updated script due to the move
    
    related to #61

--- data-raw/IHME/IHME-processing.R
+++ data-raw/IHME/IHME-processing.R
@@ -1,4 +1,5 @@
 ## reformat IHME forecasts
+# Run from data-raw/IHME
 
 #' Transform matrix of samples for one location into a quantile-format data_frame
 #'
@@ -9,7 +10,9 @@ make_qntl_dat <- function(path) {
   require(tidyverse)
   require(MMWRweek)
   require(lubridate)
-  forecast_date <- gsub("_", "-",substr(dirname(path),start=17,stop=26)) 
+  forecast_date <- gsub("_", "-",substr(dirname(path), 
+                                        start = date_offset +  1,  # these should not be hard-coded
+                                        stop  = date_offset + 10)) 
   forecast_date <- as.Date(forecast_date)
   data <- read.csv(path, stringsAsFactors = FALSE)
   # format read-in file
@@ -31,7 +34,7 @@ make_qntl_dat <- function(path) {
     dplyr::select(-names(data)[which(grepl("location",names(data))==TRUE)][-which(names(data)[which(grepl("location",names(data))==TRUE)]=="location")])
 
   ## read state code
-  state_fips_codes<-read.csv("./template/state_fips_codes.csv",stringsAsFactors = FALSE) %>%
+  state_fips_codes<-read.csv("../../template/state_fips_codes.csv",stringsAsFactors = FALSE) %>%
     dplyr::select(-"state")
   col_list1 <- c(grep("location", colnames(data)),grep("date", colnames(data)),grep("death", colnames(data)))
   death_qntl1 <- data[,c(col_list1)] %>%
@@ -119,15 +122,34 @@ make_qntl_dat <- function(path) {
 
 
 ## list all files and read
-filepaths <- list.files("./data-raw/IHME",pattern = "Hospitalization_all_locs.csv", recursive =TRUE,full.names = TRUE)
-file_processed_dates <- substr(basename(list.files("./data-processed/IHME-CurveFit",pattern = ".csv", recursive =TRUE,full.names = TRUE)),start=1,stop=10)
-raw_file_dates <- substr(dirname(filepaths),start=17,stop=26)
+filepaths <- list.files("./",pattern = "Hospitalization_all_locs.csv", recursive =TRUE,full.names = TRUE)
+file_processed_dates <- substr(basename(list.files("../../data-processed/IHME-CurveFit",
+                                                   pattern = ".csv", 
+                                                   recursive = TRUE,
+                                                   full.names = TRUE)),
+                               start = 1,
+                               stop  = 10)
+file_processed_dates <- file_processed_dates[-length(file_processed_dates)]
+
+date_offset = 3
+raw_file_dates <- substr(dirname(filepaths),
+                         start = date_offset +  1,
+                         stop  = date_offset + 10)
+
 newfile_date <- setdiff(gsub("_", "-",raw_file_dates),file_processed_dates)
+
 if (length(newfile_date)) {
   new_filepath <- filepaths[grepl(gsub("-", "_",newfile_date),filepaths)]
   for(i in 1:length(new_filepath)){
     formatted_file <- make_qntl_dat(new_filepath[i])
-    date <- gsub("_", "-",substr(dirname(new_filepath[i]),start=17,stop=26))
-    write_csv(formatted_file,path = paste0("data-processed/IHME-CurveFit/",date,"-IHME-CurveFit.csv"))
+    
+    date <- gsub("_", "-",substr(dirname(new_filepath[i]),
+                                 start = date_offset +  1,
+                                 stop  = date_offset + 10))
+    
+    write_csv(formatted_file,
+              path = paste0("../../data-processed/IHME-CurveFit/",
+                            date,
+                            "-IHME-CurveFit.csv"))
   }
 }
