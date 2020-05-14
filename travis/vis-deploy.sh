0a1d150edd9dbd346b680682ccd3a4f5779a72a2
Katie House <katiehouse3@gmail.com>
2020-05-13 22:38:54 -0400

    try only deploying trigger build

--- travis/vis-deploy.sh
+++ travis/vis-deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-git checkout gh-pages
-git fetch origin master
-git checkout origin/master  -- ./visualization/vis-master/dist
-cd ./visualization/vis-master
-cp -r ./dist/* ../../
+push_to_gh_pages() {
+  git checkout gh-pages
+  git fetch origin master
+  git checkout origin/master  -- ./visualization/vis-master/dist
+  cd ./visualization/vis-master
+  cp -r ./dist/* ../../
+  git add .
+  git commit -m "Auto Build Visualization"
+  git push https://${GH_TOKEN}@github.com/reichlab/covid19-forecast-hub.git gh-pages
+}
 
-git add .
-git commit -m "Auto Build Visualization"
-git push
+setup_git
+push_to_gh_pages
+
