447f87d65560dbfdf9bbc9be8b3fda6ce8cc6cbc
Khoale1096 <Khoale1096>
2020-05-15 01:02:34 -0400

    test zoltar 2

--- travis-main.sh
+++ travis-main.sh
@@ -45,6 +45,10 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 
    echo "Merge detected.. push to github"
    bash ./travis/push.sh
+   echo "Upload forecasts to Zoltar"
+    bash ./travis/upload-to-zoltar.sh
+    echo "Push the validated file db to Zoltar"
+    bash ./travis/push.sh
 fi
 
 # if [[ "$TRAVIS_COMMIT_MESSAGE" == *"Merge pull request"* ]]; then
