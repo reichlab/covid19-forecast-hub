b1dfdbaf59376609be7b6779ac973b1c8d36cba2
Abdul Hannan Kanji <akanji@umass.edu>
2020-07-30 16:55:00 -0400

    Update viz analytics + make numbers readable

--- visualization/vis-master/src/assets/analytics.js
+++ visualization/vis-master/src/assets/analytics.js
@@ -1,6 +1,16 @@
 // Statcounter code for Flusightnetwork.io http://flusightnetwork.io/
-global.sc_project = 12239903
+global.sc_project = 10491184
 global.sc_invisible = 1
-global.sc_security = "3c7ad044"
+global.sc_security = "40c0ea38"
 global.scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.")
 document.write("<script type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></script>")
+document.write(`<noscript>
+  <div class="statcounter">
+    <a title="shopify site analytics"
+       href="http://statcounter.com/shopify/" target="_blank">
+      <img class="statcounter"
+           src="http://c.statcounter.com/10491184/0/40c0ea38/1/"
+           alt="shopify site analytics">
+    </a>
+  </div>
+</noscript>`)
