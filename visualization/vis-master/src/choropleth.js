b71ec7bba8316d59a6a37ef262907a4c0b2f354b
Apurv <aashah@umass.edu>
2020-12-30 19:29:14 +0300

    Changing absolute numbers to relative rates

--- visualization/vis-master/src/choropleth.js
+++ visualization/vis-master/src/choropleth.js
@@ -4,10 +4,12 @@ import colormap from 'colormap'
 import textures from 'textures'
 import tinycolor from 'tinycolor2'
 import * as d3 from 'd3'
+import csv from 'csvtojson'
 
 /**
  * Return sibling data for given element
  */
+
 export const getSiblings = (element, data) => {
   let stateName = element.getAttribute('class').split(' ')[1]
   return data.filter(d => d.states.indexOf(stateName) > -1)[0]
@@ -85,7 +87,7 @@ export class ColorBar {
 
   // Update scale of colorbar
   update(range) {
-    console.log(range)
+    //console.log(range)
     this.scale.domain(range)
     let nticks = 5
     // Setup custom ticks
@@ -93,7 +95,7 @@ export class ColorBar {
       // Relative values
       nticks = 3
     }
-    console.log(range)
+    //console.log(range)
 
     let axis = d3.axisBottom(this.scale).ticks(4).tickFormat(d3.format(".2"))
 
@@ -265,7 +267,8 @@ export default class Choropleth {
    *   - Week change
    *   - Region selector change
    */
-  update(ids) {
+
+   update(ids) {
     let data = this.data
     let colorScale = this.colorScale
     let selectedTexture = this.selectedTexture
@@ -278,35 +281,103 @@ export default class Choropleth {
 
     // Update colors for given week
     data.map(d => {
-      let value = d.values[ids.weekIdx]
-      let color = '#ccc'
-      if (value !== -1) color = cmap[Math.floor(colorScale(value))]
+      const popu = {}
+      d3.csv("/static/locations.csv",function(data2) {
+        data2.forEach(function(d) {
+          popu[d.abbreviation] = d.population
+        })
+        let value = d.values[ids.weekIdx]/popu[d.states[0]]*100000
+        let color = '#ccc'
+        if (value !== -1) color = cmap[Math.floor(colorScale(value))]
 
-      d.states.map(s => {
-        let d3State = d3.select('.' + s)
+        d.states.map(s => {
+          let d3State = d3.select('.' + s)
 
-        d3State.style('fill', color)
-        d3State.attr('data-value', value)
+          d3State.style('fill', color)
+          d3State.attr('data-value', value)
 
-        if (highlightedStates.indexOf(s) > -1) {
-          // Setup selected pattern
-          let strokeColor = tinycolor(color).getLuminance() < 0.5 ?
-            'white' : '#444'
+          if (highlightedStates.indexOf(s) > -1) {
+            // Setup selected pattern
+            let strokeColor = tinycolor(color).getLuminance() < 0.5 ?
+              'white' : '#444'
 
-          d3.select('#' + selectedTexture.id() + ' rect')
-            .attr('fill', color)
+            d3.select('#' + selectedTexture.id() + ' rect')
+              .attr('fill', color)
 
-          d3.select('#' + selectedTexture.id() + ' path')
-            .style('stroke', strokeColor)
+            d3.select('#' + selectedTexture.id() + ' path')
+              .style('stroke', strokeColor)
 
-          d3State.style('stroke', strokeColor)
-            .style('stroke-opacity', 1)
-            .style('fill', selectedTexture.url())
-        } else {
-          d3State.style('stroke', 'white')
-            .style('stroke-opacity', 0)
-        }
+            d3State.style('stroke', strokeColor)
+              .style('stroke-opacity', 1)
+              .style('fill', selectedTexture.url())
+          } else {
+            d3State.style('stroke', 'white')
+              .style('stroke-opacity', 0)
+          }
+        })
       })
     })
   }
 }
+
+
+
+
+// const pop = {
+      // 'US': 328728466.0, 
+      // 'AL': 4903185.0, 
+      // 'AK': 731545.0, 
+      // 'AZ': 7278717.0, 
+      // 'AR': 3017804.0, 
+      // 'CA': 39512223.0, 
+      // 'CO': 5758736.0, 
+      // 'CT': 3565287.0, 
+      // 'DE': 973764.0, 
+      // 'DC': 705749.0, 
+      // 'FL': 21477737.0, 
+      // 'GA': 10617423.0, 
+      // 'HI': 1415872.0, 
+      // 'ID': 1787065.0, 
+      // 'IL': 12671821.0, 
+      // 'IN': 6732219.0, 
+      // 'IA': 3155070.0, 
+      // 'KS': 2913314.0, 
+      // 'KY': 4467673.0, 
+      // 'LA': 4648794.0, 
+      // 'ME': 1344212.0, 
+      // 'MD': 6045680.0, 
+      // 'MA': 6892503.0, 
+      // 'MI': 9986857.0, 
+      // 'MN': 5639632.0, 
+      // 'MS': 2976149.0, 
+      // 'MO': 6626371.0, 
+      // 'MT': 1068778.0, 
+      // 'NE': 1934408.0, 
+      // 'NV': 3080156.0, 
+      // 'NH': 1359711.0, 
+      // 'NJ': 8882190.0, 
+      // 'NM': 2096829.0, 
+      // 'NY': 19453561.0, 
+      // 'NC': 10488084.0, 
+      // 'ND': 762062.0, 
+      // 'OH': 11689100.0, 
+      // 'OK': 3956971.0, 
+      // 'OR': 4217737.0, 
+      // 'PA': 12801989.0, 
+      // 'RI': 1059361.0, 
+      // 'SC': 5148714.0, 
+      // 'SD': 884659.0, 
+      // 'TN': 6829174.0, 
+      // 'TX': 28995881.0, 
+      // 'UT': 3205958.0, 
+      // 'VT': 623989.0, 
+      // 'VA': 8535519.0, 
+      // 'WA': 7614893.0, 
+      // 'WV': 1792147.0, 
+      // 'WI': 5822434.0, 
+      // 'WY': 578759.0, 
+      // 'AS': 55641.0, 
+      // 'GU': 164229.0, 
+      // 'MP': 55144.0, 
+      // 'PR': 3754939.0,  
+      // 'VI': 107268.0}
