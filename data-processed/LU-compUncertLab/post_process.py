4f0843356f981051eb74e4d58bc8480cb495fb82
Matthew Piriya <piriya.matt@gmail.com>
2022-03-07 11:22:55 -0500

    script to clip negative forecast values to zero

--- data-processed/LU-compUncertLab/post_process.py
+++ data-processed/LU-compUncertLab/post_process.py
@@ -7,5 +7,6 @@ import pandas as pd
 from interface import interface
 
 if __name__ == "__main__":
-
     io = interface(0)
+    forecast = io.grab_recent_forecast_file()
+    forecast['value'] = forecast['value'].clip(lower=0)
