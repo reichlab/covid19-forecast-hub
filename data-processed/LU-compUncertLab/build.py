b21fba9ae499623ba2c5da77005028e629ab10aa
parthdg420 <98370984+parthdg420@users.noreply.github.com>
2022-03-11 15:05:03 -0500

    Update build.py

--- data-processed/LU-compUncertLab/build.py
+++ data-processed/LU-compUncertLab/build.py
@@ -2,21 +2,27 @@
 
 from interface import interface
 from model import VAR
+import argparse
 
 if __name__ == "__main__":
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--LOCATION', type=str)
+    args = parser.parse_args()
+    location = args.LOCATION
     io = interface(0)
-    for n,location in enumerate(io.locations):
-        io = interface(0)
+    
+    print(location)
+    io = interface(0)
         
-        io.subset2locations([location])
+    io.subset2locations([location])
 
-        forecast_model = VAR(io.modeldata)
-        forecast_model.fit()
+    forecast_model = VAR(io.modeldata)
+    forecast_model.fit()
 
-        io.formatSamples( forecast_model)
-        io.un_center() # multipy by std and add back running mean
+    io.formatSamples( forecast_model)
+    io.un_center() # multipy by std and add back running mean
 
-        io.fromSamples2Quantiles()
+    io.fromSamples2Quantiles()
 
-        io.writeout(n)
+    io.writeout(n)
+
