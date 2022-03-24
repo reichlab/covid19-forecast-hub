5f75911677cfae2b1dca3c0ffecd461e5a039426
Matthew Piriya <piriya.matt@gmail.com>
2022-03-24 11:46:21 -0400

    fixed dumb error in produceListofruns

--- data-processed/LU-compUncertLab/produceListOfRUNS.py
+++ data-processed/LU-compUncertLab/produceListOfRUNS.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
+	cases = pd.read_csv("../../data-truth/truth-Incident Cases.csv")
 	locs = cases.location.astype(str).unique()
 	f = open("RUNS.csv", "w")
 	for l in locs:
