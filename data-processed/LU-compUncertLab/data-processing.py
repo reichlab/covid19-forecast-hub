c197aaad0652638837f75dff5951a6d2a620bb64
Matthew Piriya <piriya.matt@gmail.com>
2022-03-31 14:51:37 -0400

    normalized location so that merge actually works on county level

--- data-processed/LU-compUncertLab/data-processing.py
+++ data-processed/LU-compUncertLab/data-processing.py
@@ -16,13 +16,13 @@ class dataprep(object):
         
         # we need to separate cases at the state level from cases at county level
         cases_counties = cases.loc[ [True if len(_)>=4 else False for _ in cases.location] ,:]
-        
+
         cases_counties = cases_counties.rename(columns = {"value":"cases"})
         cases = cases.rename(columns = {"value":"cases"})
         self.cases = cases
 
         # strips the last three digits from the FIPS to get either 1 or 2 digits, then left fills with 0 if 1 digit
-        state_codes = cases_counties.apply(lambda x: str(x['location'])[:-3].zfill(2), axis=1)
+        state_codes = cases_counties.apply(lambda x: x['location'][:-3].zfill(2), axis=1)
 
         # appends to the dataframe
         cases_counties.insert(len(cases_counties.columns), 'state', state_codes)
@@ -35,6 +35,11 @@ class dataprep(object):
         deaths.location = deaths.location.astype(str)
 
         deaths = deaths.rename(columns = {"value":"deaths"})
+
+        # NOTE: THIS IS A VERY EXPENSIVE OPERATION
+        normalize_loc = deaths.apply(lambda x: x['location'].zfill(2), axis=1)
+        deaths.insert(len(deaths.columns), 'nloc', normalize_loc)
+
         self.deaths = deaths
 
     def load_hosps(self):
@@ -43,6 +48,11 @@ class dataprep(object):
         hosps.location = hosps.location.astype(str)
         
         hosps = hosps.rename(columns = {"value":"hosps"})
+
+        # NOTE: THIS IS A VERY EXPENSIVE OPERATION
+        normalize_loc = hosps.apply(lambda x: x['location'].zfill(2), axis=1)
+        hosps.insert(len(hosps.columns), 'nloc', normalize_loc)
+
         self.hosps = hosps
 
     def merge(self):
@@ -55,10 +65,10 @@ class dataprep(object):
 
     def mergeCounties(self):
         # merge the county cases with state deaths
-        d = self.ccases_counties.merge(self.deaths, left_on=['date', 'state'], right_on=['date', 'location'], suffixes=(None, "_d"))
+        d = self.ccases_counties.merge(self.deaths, left_on=['date', 'state'], right_on=['date', 'nloc'], suffixes=(None, "_d"))
         # merge the county cases with state hosps
-        d = d.merge(self.hosps, left_on=['date', 'state'], right_on=['date', 'location'], suffixes=(None, "_h"))
-        d = d.drop(columns=['state', 'location_d', 'location_name_d', 'location_h', 'location_name_h'])
+        d = d.merge(self.hosps, left_on=['date', 'state'], right_on=['date', 'nloc'], suffixes=(None, "_h"))
+        d = d.drop(columns=['state', 'location_d', 'location_name_d', 'nloc', 'location_h', 'location_name_h', 'nloc_h'])
 
         self.threestreams_county = d
         return d
