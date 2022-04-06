1d2a85765c02e338373eb50463d7a8c14509a4ba
Matthew Piriya <piriya.matt@gmail.com>
2022-04-05 21:39:24 -0400

    script to compile quantile predictions into one csv for final output

---
+++ data-processed/LUcompUncertLab-VAR_3streams/compilePredictions.py
@@ -0,0 +1,14 @@
+from interface import interface
+from glob import glob
+import pandas as pd
+
+if __name__ == "__main__":
+	io = interface(0)
+	final_filename = "{:s}-LUcompUncertLab-VAR_3streams.csv".format(io.forecast_date)
+
+	for n, file in enumerate(glob('./location_specific_forecasts/{:s}_LUcompUncertLab-quantiles*'.format(io.forecast_date))):
+		curr = pd.read_csv(file, compression='.gz')
+		if n == 0:
+			curr.to_csv(final_filename, index=False)
+		else:
+			curr.to_csv(final_filename, index=False, mode='a', header=False)
