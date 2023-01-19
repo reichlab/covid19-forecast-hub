aa9421d2413a1f2bbd888da37d3e6e9d0e232ebf
matthewcornell <matt@matthewcornell.org>
2023-01-19 08:06:50 -0500

    [upload_covid19_forecasts_to_zoltar.py updates validated_file_db.json in spite of failed upload jobs #8095]: changed upload_covid19_forecasts_to_zoltar.py to handle the case where a forecast is uploaded OK but its job fails, and to only update db object/file if a job succeeds

--- code/test/test_test_formatting.py
+++ code/test/test_test_formatting.py
@@ -1,10 +1,9 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest import skip
 
 
 class TestFormattingTestCase(unittest.TestCase):
 
+    @skip('TODO. Add tests for test_formatting.py class')
     def test_check_formatting(self):
-        # TODO: Add a test for the data validation here
-        print('TODO. Add tests for test_formatting.py class')
         pass
